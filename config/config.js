319a936a59566cdf949f89aa2f69a742eed54db8
Harish Matta <harish@flowace.in>
2019-02-16 14:42:24 +0530

    added env variables

--- config/config.js
+++ config/config.js
@@ -1,11 +1,29 @@
 'use strict'
 
-const config = require('./config.json')
+const config = {
+  "development": {
+    "host": "localhost",
+    "user": "root",
+    "password": "matta",
+    "database": "plivoc",
+    "secret": "plivo_contact",
+    "connectionLimit": 15,
+    "tokenExpiry": 86400
+  },
+  "production": {
+    "host": process.env.DATABASE_URL,
+    "user": process.env.USER,
+    "password": process.env.PASSWORD
+  }
+}
+
 const { NODE_ENV = 'development' } = process.env
 
 const environmentConfig = config[NODE_ENV]
 const defaultConfig = config.development
 const finalConfig = Object.assign(defaultConfig, environmentConfig)
+
+console.log(finalConfig)
 
 //setting global config
 global.config = finalConfig
